6a6894291ee8e012049aab542ad9454bae123bf4
minato <ryan.wang@tradevan.com.tw>
2021-03-11 17:07:46 +0800

    chore: add plugins done

--- shell/install.sh
+++ shell/install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+## create namespace
+echo '===> create namespace' $1
 kubectl create namespace $1
 
 ## helm add repo jenkins
-helm repo add jenkins https://charts.jenkins.io
+#helm repo add jenkins https://charts.jenkins.io
 
 ## install jenkins in newnamespace
-echo 'install jenkins in' $1
-#helm install jenkins jenkins/jenkins -n $1  --set service.type=ClusterIP
+echo '===> install jenkins in' $1
+helm install jenkins jenkins/jenkins -n $1 --set service.type=ClusterIP --values ./jks.yaml
 
 ## get secert with admin pwd
-echo $(kubectl get secret --namespace jenkins jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode)
+echo '===> admin secret is '
+echo $(kubectl get secret --namespace $1 jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode)
+
+## check all svc under ns
+kubectl get all -n $1
 
 ## set svc to get public ip
 kubectl patch svc jenkins -n $1 -p '{"spec": {"type": "LoadBalancer"}}'
--- shell/killns.sh
+++ shell/killns.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ## kill all svc under namespace
+echo '===> killing svc under and namespcae ' $1
 kubectl delete ns $1
 
